7f4eb9e588dd694836f905e9964a77dbc64cf686
Ian Voorhies <33384413+ian-voorhies@users.noreply.github.com>
2017-11-17 17:48:09 -0600

    Create ReplyCodes.java

---
+++ ReplyCodes.java
@@ -0,0 +1,8 @@
+public class ReplyCodes{
+	public static final int _NEW=0;
+	public static final int _SEARCH=1;
+	public static final int _EXIT=2;
+	public static final int _FAIL=0;
+	public static final int _SUCCESS=1;
+	public static final int _FULL=2;
+}
